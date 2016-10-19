6203c2811abb1af835825fb469665828d32481fb
Saran Rungworawattanakul <za-12unlaby@outlook.com>
2016-10-19 13:18:13 +0700

    s

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
@@ -14,8 +12,7 @@ app.use(express.static(__dirname + '/public'));
 // set the home page route
 app.get('/', function (req, res) {
     
-    // ejs render automatically looks in the views folder
-    res.render('index');
+    res.send('Hello World!');
 });
 
 app.listen(port, function () {
