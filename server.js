c753fb0f499dc8b476cde2aea37f63d847908f47
Saran Rungworawattanakul <za-12unlaby@outlook.com>
2016-10-19 15:17:47 +0700

    1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function (req, res) {
-    
+    console.log(req);
     res.send('Hello World!');
 });
 app.post('/', function (req, res) {
